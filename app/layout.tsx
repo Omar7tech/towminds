6e0e92aca6015c66f9c3a450f405d067e08e2556
Omar Abi Farraj <omar.7tech@gmail.com>
2026-09-11 23:33:56 +0300

    ad f

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const neueMachina = localFont({
+  variable: "--font-neue-machina",
+  src: [
+    { path: "./fonts/NeueMachina-Light.otf", weight: "300", style: "normal" },
+    { path: "./fonts/NeueMachina-Regular.otf", weight: "400", style: "normal" },
+    { path: "./fonts/NeueMachina-Ultrabold.otf", weight: "800", style: "normal" },
+  ],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +36,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`dark ${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} h-full antialiased`}
+      className={`dark ${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} ${neueMachina.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
